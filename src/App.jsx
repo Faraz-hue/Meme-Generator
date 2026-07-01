36c7735b87f1cd25f776e17580837eb4dd8224e5
Faraz-hue <mfarazazam9@gmail.com>
2026-07-01 11:26:40 +0500

    Learned the side effect or use effect that is used for outside component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
-
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-import Header from './components/Header'
-import Body from './components/Body'
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Body from "./components/Body";
 
 function App() {
+  const [starWarsData, setStarWarsData] = React.useState(null);
+  const [count, setCount] = React.useState(0)
+  React.useEffect(() => {
+    console.log("Use Side effet")
+    fetch(`https://swapi.dev/api/people/${count}`)
+      .then((res) => res.json())
+      .then((data) => setStarWarsData(data));
+  }, [count]); // Empty dependency array means run only once
+  console.log("rendered")
+
+  const [allMemes, setAllMemes] = React.useState([])
+  useEffect(() => {
+    fetch(`https://api.imgflip.com/get_memes`)
+      .then(res => res.json())
+      .then(data => console.log(data.data.memes))
+  }, [])
 
   return (
     <>
       <Header />
       <Body />
+      <button className="btnCharacter" onClick={() => setCount(prevCount => prevCount + 1)}>Get the next Character</button>
+      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
